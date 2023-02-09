08eb139dd6bc6c181bcd42f5b7c5a9921d2f3bfc
patriot720 <cerkin-3@yandex.ru>
2023-02-09 21:06:03 +0300

    update 21:06 Thu, 09 Feb

--- modules/fish/functions/sync_repo.fish
+++ modules/fish/functions/sync_repo.fish
@@ -1,6 +1,6 @@
 function sync_repo -a path
     cd $path;
     git_update;
-    git submodule foreach 'cd $toplevel/$path;fish -c "git_update";cd -;';
+    git submodule foreach --recursive 'cd $toplevel/$path;fish -c "git_update";cd -;';
     cd -;
 end
