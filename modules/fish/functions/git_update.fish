6f1010974943ed5ef33cd86e34537daf5f71db09
patriot720 <cerkin-3@yandex.ru>
2023-02-09 21:05:37 +0300

    update 21:05 Thu, 09 Feb

--- modules/fish/functions/git_update.fish
+++ modules/fish/functions/git_update.fish
@@ -1,5 +1,8 @@
 function git_update
-    git add .;
-    git commit -m "update $(date "+%H:%M %a, %d %b")";
-    git push origin;
+    if git remote get-url origin | grep 'p2d0';
+        echo "commiting";
+        git add .;
+        git commit -m "update $(date "+%H:%M %a, %d %b")";
+        git push origin;
+    end
 end
--- modules/fish/functions/sync_repo.fish
+++ modules/fish/functions/sync_repo.fish
@@ -1,6 +1,6 @@
 function sync_repo -a path
     cd $path;
     git_update;
-    git submodule foreach "(git remote get-url origin | grep p2d0) && (git add .;git commit -m 'update $(date "+%H:%M %a, %d %b")';git push origin)";
+    git submodule foreach 'cd $toplevel/$path;fish -c "git_update";cd -;';
     cd -;
 end
