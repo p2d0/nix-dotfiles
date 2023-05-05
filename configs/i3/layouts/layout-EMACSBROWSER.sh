fe4e80e976baaaac29a282ff16765e70378c7218
patriot720 <cerkin-3@yandex.ru>
2023-05-05 20:00:08 +0300

    update 20:00 Fri, 05 May

--- configs/i3/layouts/layout-EMACSBROWSER.sh
+++ configs/i3/layouts/layout-EMACSBROWSER.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-firefox -new-window https://chat.openai.com
+# firefox -new-window https://chat.openai.com
+firefox -new-window https://chat.forefront.ai
--- modules/nixos/editors/.doom.d/configuration/autodarkmode.el
+++ modules/nixos/editors/.doom.d/configuration/autodarkmode.el
@@ -9,7 +9,27 @@
     (let ((scheme (car values)))
       (cond
         ((= 1 scheme)
-          (load-theme +dark-theme+ t)) ;; my custom function that sets a dark theme
+					(custom-set-faces!
+						;; '(window-divider   ((t (:foreground "#1E2029" :background "#1E2029"))))
+						;; '(solaire-default-face   ((t (:background "#1E2029"))))
+						;; '(internal-border   ((t (:foreground "#1E2029" :background "#1E2029"))))
+
+						;; (window-divider :foreground "#1E2029" :background "#1E2029")
+						;; (solaire-default-face :background "#1E2029")
+						;; (internal-border :foreground "#1E2029" :background "#1E2029")
+						;; (doom-nano-modeline-evil-emacs-state-face    :foreground "#1E2029" :background "#1E2029")
+						;; (doom-nano-modeline-evil-insert-state-face   :foreground "#1E2029" :background "#1E2029")
+						;; (doom-nano-modeline-evil-motion-state-face   :foreground "#1E2029" :background "#1E2029")
+						;; (doom-nano-modeline-evil-normal-state-face   :foreground "#1E2029" :background "#1E2029")
+						;; (doom-nano-modeline-evil-operator-state-face :foreground "#1E2029" :background "#1E2029")
+						;; (doom-nano-modeline-evil-replace-state-face  :foreground "#1E2029" :background "#1E2029")
+						;; (doom-nano-modeline-evil-visual-state-face   :foreground "#1E2029" :background "#1E2029")
+						;; (doom-nano-modeline-inactive-face            :foreground "#1E2029" :background "#1E2029")
+						)
+
+          (load-theme +dark-theme+ t)
+
+					) ;; my custom function that sets a dark theme
         ((= 2 scheme)
           (load-theme +light-theme+ t)) ;; 1000 internet points to whoever guesses what this does
         (t (message "I don't know how to handle scheme: %s" scheme))))))
@@ -20,3 +40,4 @@
   "org.freedesktop.impl.portal.Settings"
   "SettingChanged"
   #'theme--handle-dbus-event)
+
