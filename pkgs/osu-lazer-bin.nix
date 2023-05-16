118ad5f3693b3db209f7582299027472001bbb8a
patriot720 <cerkin-3@yandex.ru>
2023-05-16 20:00:12 +0300

    update 20:00 Tue, 16 May

--- pkgs/osu-lazer-bin.nix
+++ pkgs/osu-lazer-bin.nix
@@ -2,11 +2,11 @@
 
 appimageTools.wrapType2 rec {
   pname = "osu-lazer-bin";
-  version = "2023.403.1";
+  version = "2023.513.0";
 
   src = fetchurl {
     url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-    sha256 = "sha256-xHz9CYpI02m54xJOkO8Kd7KIxvAELqCDOZnw05EsSYw=";
+    sha256 = "sha256-EMGFi4TB45UkXiGMdg7awvhoyoYoRC+zo5fWvYMOf9c=";
   };
 
   extraPkgs = pkgs: with pkgs; [ icu ];
