de836f2909152a110cc4c83f5446ffc92adfe494
patriot720 <cerkin-3@yandex.ru>
2023-10-12 20:00:20 +0300

    update 20:00 Thu, 12 Oct

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "An example NixOS configuration";
 
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-22.11"; };
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-23.05"; };
     nixos-unstable.url = "nixpkgs/nixos-unstable";
     nixos-unstable-small.url = "nixpkgs/nixos-unstable-small";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
     hyprland.url = "github:hyprwm/Hyprland";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
