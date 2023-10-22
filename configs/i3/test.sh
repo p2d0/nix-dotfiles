711ec5aeda78a0955cca3fb3dfebbbd7ac4d527f
patriot720 <cerkin-3@yandex.ru>
2023-10-22 20:00:12 +0300

    update 20:00 Sun, 22 Oct

--- configs/i3/test.sh
+++ configs/i3/test.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 sleep 1
 setxkbmap us
-for i in {1..10}; do
+for i in {1..500}; do
     xdotool type --clearmodifiers --delay 0 -- "/imagegen"
     sleep 0.001
     xdotool key Return
     sleep 0.001
     xdotool key Shift+Tab
+    sleep 6
 done
 setxkbmap -layout us,ru -option -option "grp:alt_shift_toggle";
