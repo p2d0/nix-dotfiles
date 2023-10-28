21b89cc5ad0aff03549e0f667f317869f5896133
patriot720 <cerkin-3@yandex.ru>
2023-10-28 20:00:29 +0300

    update 20:00 Sat, 28 Oct

--- pkgs/osu-lazer-bin.nix
+++ pkgs/osu-lazer-bin.nix
@@ -2,7 +2,7 @@
 
 appimageTools.wrapType2 rec {
   pname = "osu-lazer-bin";
-  version = "2023.924.1";
+  version = "2023.1026.0";
 
   src = fetchurl {
     url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
