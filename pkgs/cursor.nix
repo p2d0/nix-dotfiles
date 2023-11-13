5d12f1666dc47522a057ca5ae177da685e97aad3
patriot720 <cerkin-3@yandex.ru>
2023-11-13 20:00:11 +0300

    update 20:00 Mon, 13 Nov

--- pkgs/cursor.nix
+++ pkgs/cursor.nix
@@ -1,12 +1,12 @@
-# { appimageTools, lib, fetchurl }:
+{ appimageTools, lib, fetchurl }:
 
-with import <nixos-unstable> {};
+# with import <nixos-unstable> {};
 appimageTools.wrapType2 rec {
-  pname = "axiomai";
-  version = "1.0.0";
+  pname = "cursor";
+  version = "0.8.4";
 
   src = fetchurl {
-    url = "https://axiom.ai/desktop_app/axiom-desktop-linux-latest.AppImage";
-    sha256 = "sha256-sw2Q/LtJKfoW2mUx2rya2l6OFqHrKPVw2BDbk1+Ejn4=";
+    url = "https://dl.todesktop.com/230313mzl4w4u92/linux/appImage/x64";
+    sha256 = "";
   };
 }
