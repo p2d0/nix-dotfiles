ac7ac42f36fbcd75c20b0c2662f04e55312d892b
patriot720 <cerkin-3@yandex.ru>
2023-11-24 20:00:11 +0300

    update 20:00 Fri, 24 Nov

--- home.nix
+++ home.nix
@@ -84,30 +84,22 @@ in {
       home.packages = [
       ];
 
-      # programs.fish.shellInit = ''
-      #   function rebuild-work
-      #     if [ "$USER" != "root" ]
-      #       echo "This function requires root privileges."
-      #     else
-      #       nixos-rebuild switch --impure  --flake '/etc/nixos/.?submodules=1#mysystem' -j6 $argv
-      #       /run/current-system/specialisation/work/activate
-      #     end
-      #   end
-      #   function activate-specialisation-work
-      #     /run/current-system/specialisation/work/activate
-      #   end
+      programs.fish.shellInit = ''
+        function rebuild-work
+          sudo nixos-rebuild switch --impure  --flake '/etc/nixos/.?submodules=1#mysystem' -j6 $argv
+          sudo /run/current-system/specialisation/work/activate
+        end
+        function activate-specialisation-work
+          sudo /run/current-system/specialisation/work/activate
+        end
 
-      #   function rebuild-default
-      #     if [ "$USER" != "root" ]
-      #       echo "This function requires root privileges."
-      #     else
-      #       nixos-rebuild switch --impure  --flake '/etc/nixos/.?submodules=1#mysystem' -j6 $argv \
-      #       /run/current-system/specialisation/default/activate
-      #     end
-      #   end
-      #   function activate-specialisation-default
-      #     sudo /run/current-system/specialisation/default/activate
-      #   end'';
+        function rebuild-default
+          sudo nixos-rebuild switch --impure  --flake '/etc/nixos/.?submodules=1#mysystem' -j6 $argv
+          sudo /run/current-system/specialisation/default/activate
+        end
+        function activate-specialisation-default
+          sudo /run/current-system/specialisation/default/activate
+        end'';
 
       qt = {
         enable = true;
