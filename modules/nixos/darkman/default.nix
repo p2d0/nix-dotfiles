2fd4db4278f771f8c6e36fb30567b02289225a70
patriot720 <cerkin-3@yandex.ru>
2024-01-18 20:00:11 +0300

    update 20:00 Thu, 18 Jan

--- modules/nixos/darkman/default.nix
+++ modules/nixos/darkman/default.nix
@@ -23,27 +23,9 @@ in {
         packages = [pkgs.my.darkman];
       };
     };
-    # systemd.user.services.darkman = {
-    #   Unit = {
-    #     After = [ "default.target" ];
-    #     PartOf = [ "default.target" ];
-    #   };
-    # };
-
-    systemd.user.services.dark = {
-      enable = true;
-      description = "Darkman";
-      wantedBy = [ "default.target" ];
-      serviceConfig = {
-        ExecStart =
-          "${pkgs.my.darkman}/bin/darkman run";
-        Environment = ["PATH=/run/current-system/sw/bin"];
-      };
-    };
-
-    # systemd.packages = [
-    #   pkgs.my.darkman
-    # ];
+    systemd.packages = [
+      pkgs.my.darkman
+    ];
     environment.systemPackages = [
       pkgs.my.darkman
     ];
