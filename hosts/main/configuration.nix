9ee08797c4aabdc5b43e0745e73e3dbaf62131c4
patriot720 <cerkin-3@yandex.ru>
2024-02-15 20:00:10 +0300

    update 20:00 Thu, 15 Feb

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -243,6 +243,7 @@
         "30 20 * * * andrew fish -c 'update-system'"
         "00 23 * * * root sh -c 'shutdown now'"
         "00 22 * * * root sh -c 'shutdown now'"
+        "00 21 * * * root sh -c 'shutdown now'"
         "00 20 * * * andrew fish -c 'sync_repos'"
         "3,8,13,18,23,28,33,38,43,48,53,58 * * * * andrew sleep 50 ; wget --no-check-certificate -O - https://freedns.afraid.org/dynamic/update.php?RnBTMHFiQlhHWnVmUXpNYmtLWlQ0ZXB5OjIxNjg5NzI5 >> /tmp/freedns_ug_kyrgyzstan_kg.log 2>&1 &"
       ];
