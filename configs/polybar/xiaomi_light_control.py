61b11faee2ef6df397f60cf139c44392a23e1cc0
patriot720 <cerkin-3@yandex.ru>
2024-02-25 20:00:12 +0300

    update 20:00 Sun, 25 Feb

--- configs/polybar/xiaomi_light_control.py
+++ configs/polybar/xiaomi_light_control.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Import the necessary modules
 from miio import Yeelight
+from miio.exceptions import DeviceException
 from sys import argv
 from time import sleep
 import os
@@ -23,7 +24,7 @@ yeelights = [
 def create_yeelight(lightbulb):
     ping(lightbulb["ip"])
     yeelight = Yeelight(lightbulb["ip"], lightbulb["token"], model=lightbulb["model"])
-    yeelight.timeout = 50;
+    yeelight.timeout = 5000;
     return yeelight
 
 # Define a function to get the brightness of a lightbulb
@@ -34,18 +35,26 @@ def get_brightness(lightbulb):
 
 # Define a function to set the brightness of a lightbulb
 def set_brightness(lightbulb, brightness):
-    yeelight = create_yeelight(lightbulb)
-    yeelight.set_brightness(brightness)
-    updated_brightness = get_brightness(lightbulb)
+    try:
+        yeelight = create_yeelight(lightbulb)
+        yeelight.set_brightness(brightness)
+        updated_brightness = get_brightness(lightbulb)
+    except DeviceException:
+        sleep(0.5)
+        set_brightness(lightbulb, brightness)
     if(updated_brightness != brightness):
         sleep(0.5)
         set_brightness(lightbulb, brightness)
 
 # Define a function to set the color temperature of a lightbulb
 def set_temperature(lightbulb, temperature):
-    yeelight = create_yeelight(lightbulb)
-    yeelight.set_color_temp(temperature)
-    updated_temperature = int(yeelight.get_properties(["ct"])[0])
+    try:
+        yeelight = create_yeelight(lightbulb)
+        yeelight.set_color_temp(temperature)
+        updated_temperature = int(yeelight.get_properties(["ct"])[0])
+    except DeviceException:
+        sleep(0.5)
+        set_temperature(lightbulb, temperature)
     if(updated_temperature != temperature):
         sleep(0.5)
         set_temperature(lightbulb, temperature)
@@ -59,14 +68,26 @@ def get_power_status(lightbulb):
 # Define a function to toggle a lightbulb on and off
 def toggle_light(lightbulb):
     yeelight = create_yeelight(lightbulb)
-    current_state = yeelight.get_properties(["power"])[0]
-    yeelight.toggle()  # Toggle the light state
+    try:
+        current_state = yeelight.get_properties(["power"])[0]
+        yeelight.toggle()  # Toggle the light state
+    except DeviceException:
+        sleep(0.5);
+        toggle_light(lightbulb)
     new_state = yeelight.get_properties(["power"])[0]
     print(new_state,current_state);
     if(current_state == new_state):
         sleep(0.5);
         toggle_light(lightbulb)
     return current_state, new_state
+
+def enable_light(lightbulb):
+    yeelight = create_yeelight(lightbulb)
+    try:
+        yeelight.on()  # Toggle the light state
+    except DeviceException:
+        sleep(0.5);
+        enable_light(lightbulb)
 
 # Define a function to display the brightness of the lightbulbs
 def display_brightness():
@@ -125,6 +146,11 @@ def toggle_lights():
     for i, (current_state, new_state) in enumerate(states):
         print(f"Light {i + 1} toggled: {current_state} -> {new_state}")
 
+def enable_lights():
+    states = [enable_light(lightbulb) for lightbulb in yeelights]
+    # for i in enumerate(states):
+    #     print(f"Light {i + 1} toggled: {current_state} -> {new_state}")
+
 # Check if the script is run with the correct number of arguments
 if len(argv) > 2:
     arg1 = argv[1]
@@ -146,5 +172,7 @@ elif len(argv) > 1:
         display_temperature()
     elif arg1 == "toggle":
         toggle_lights()
+    elif arg1 == "enable":
+        enable_lights()
 else:
     print("Usage: python polybar_yeelight.py <display_brightness|display_temp|change|temperature|toggle> <amount|temperature>")
