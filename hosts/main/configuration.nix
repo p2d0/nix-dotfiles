3cd65ef0854e0bd07c85d8f5be525fb96c4f3369
patriot720 <cerkin-3@yandex.ru>
2024-03-03 20:00:14 +0300

    update 20:00 Sun, 03 Mar

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -240,7 +240,7 @@
       enable = true;
       systemCronJobs = [
         "*/30 11-21 * * * andrew DBUS_SESSION_BUS_ADDRESS='unix:path=/run/user/1000/bus' /run/current-system/sw/bin/notify-send 'Update daily'"
-        "30 20 * * * andrew fish -c 'update-system'"
+        # "30 20 * * * andrew fish -c 'update-system'"
         "00 23 * * * root sh -c 'shutdown now'"
         "00 22 * * * root sh -c 'shutdown now'"
         "00 21 * * * root sh -c 'shutdown now'"
@@ -452,7 +452,7 @@
       calibre
       xorg.xdpyinfo
       # postman
-      brave
+      unstable.brave
       peco
       ffmpeg-full
       dfeet
