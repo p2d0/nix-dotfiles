0b1a1dce57e4861f63187888ef0240d313b8d8b1
patriot720 <cerkin-3@yandex.ru>
2024-11-25 20:00:11 +0300

    update 20:00 Mon, 25 Nov

--- pkgs/cursor.nix
+++ pkgs/cursor.nix
@@ -2,13 +2,14 @@
 
 let
   pname = "cursor";
-  version = "0.40.4";
+  version = "0.43.3";
   appKey = "230313mzl4w4u92";
-  buildKey = "2409052yfcjagw2";
+  buildKey = "24111460bf2loz1";
 
   src = pkgs.fetchurl {
-    url = "https://download.todesktop.com/${appKey}/cursor-${version}-build-${buildKey}-x86_64.AppImage";
-    hash = "sha256-ZURE8UoLPw+Qo1e4xuwXgc+JSwGrgb/6nfIGXMacmSg=";
+    # url = "https://download.todesktop.com/${appKey}/cursor-${version}-build-${buildKey}-x86_64.AppImage";
+    url = "https://dl.todesktop.com/230313mzl4w4u92/linux/appImage/x64";
+    hash = "sha256-GK0DFxBmu97aK/skKi+W/T3xMNZ5HfKn67an7TXQ+gY=";
   };
   appimageContents = appimageTools.extract { inherit pname version src; };
 in
