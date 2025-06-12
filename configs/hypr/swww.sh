eb02605fef61dd209c3e6a2d46a4e5804db18b62
patriot720 <cerkin-3@yandex.ru>
2025-06-12 20:00:13 +0300

    update 20:00 Thu, 12 Jun

---
+++ configs/hypr/swww.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+if [ "$(darkman get)" = "dark" ]; then
+    swww img /etc/nixos/bg_old.png --transition-type center --transition-fps 75
+else
+    swww img /etc/nixos/light.jpg --transition-type center --transition-fps 75
+fin
