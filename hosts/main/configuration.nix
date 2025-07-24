36b76bc34a4cde24513830d380cb04da8188edd4
patriot720 <cerkin-3@yandex.ru>
2025-07-24 20:00:13 +0300

    update 20:00 Thu, 24 Jul

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -846,6 +846,14 @@ run-shell ${pkgs.tmuxPlugins.yank}/share/tmux-plugins/yank/yank.tmux
       nwg-drawer
       keepassxc
       remmina
+      (pkgs.whisper-cpp.overrideAttrs (oldAttrs: {
+    src = pkgs.fetchFromGitHub {
+      owner = "ggml-org";
+      repo = "whisper.cpp";
+      rev = "a8d002cfd879315632a579e73f0148d06959de36";
+      sha256 = "sha256-dppBhiCS4C3ELw/Ckx5W0KOMUvOHUiisdZvkS7gkxj4=";
+    };
+  }))
       unstable.aider-chat
       frida-tools
       ghidra-bin
