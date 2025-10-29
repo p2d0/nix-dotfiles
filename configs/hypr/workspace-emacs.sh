825301c19dd94f52d4271c1922a64c2e2a39506a
patriot720 <cerkin-3@yandex.ru>
2025-10-29 20:00:13 +0300

    update 20:00 Wed, 29 Oct

--- configs/hypr/workspace-emacs.sh
+++ configs/hypr/workspace-emacs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 emacsclient -c -F "'(name . \"emacs-todo\"))" --eval "(emacs-todo)" &
-brave --new-window --app=https://calendar.notion.so/
+brave --disable-features=WaylandWpColorManagerV1 --new-window --app=https://calendar.notion.so/
