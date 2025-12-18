0d2ddbda4594fa83b69267e26557c31ff643ed0f
patriot720 <cerkin-3@yandex.ru>
2025-12-18 20:00:13 +0300

    update 20:00 Thu, 18 Dec

--- pkgs/osu-lazer-bin.nix
+++ pkgs/osu-lazer-bin.nix
@@ -7,7 +7,7 @@
 
 let
   pname = "osu-lazer-bin";
-  version = "2025.1209.0-lazer";
+  version = "2025.1218.0-lazer";
 
   src = {
     aarch64-darwin = fetchzip {
@@ -22,7 +22,7 @@ let
     };
     x86_64-linux = fetchurl {
       url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-      hash = "sha256-42Fx3CxLcW97cxRXXCqPJ8NzRBnpi62IjxGLnSRkO4g=";
+      hash = "sha256-9TLdClS9+bDt0/cONsQjB5nUmis+rWOo5JAtw4oeGU0=";
     };
   }.${stdenv.system} or (throw "${pname}-${version}: ${stdenv.system} is unsupported.");
 
