32901caffe1923c59b39ed3d234b9aac7e4fde2e
patriot720 <cerkin-3@yandex.ru>
2026-06-13 20:00:12 +0300

    update 20:00 Sat, 13 Jun

--- pkgs/cassistant/default.nix
+++ pkgs/cassistant/default.nix
@@ -1,5 +1,6 @@
 { buildFHSEnv, dpkg, gtk2, glib, gdk-pixbuf, pango, cairo, atk, dbus, libX11
-, libXi, libXext, libXfixes, libXtst, sqlite, xorg, lib, stdenv, fetchurl }:
+, libXi, libXext, libXfixes, libXtst, sqlite, xorg, lib, stdenv, fetchurl
+, makeDesktopItem, symlinkJoin }:
 
 let
   extracted = stdenv.mkDerivation {
@@ -22,34 +23,69 @@ let
 
     meta.license = lib.licenses.unfree;
   };
-in buildFHSEnv {
-  pname = "cassistant";
-  version = "6.5-1";
 
-  targetPkgs = pkgs: [
-    gtk2
-    glib
-    gdk-pixbuf
-    pango
-    cairo
-    atk
-    dbus
-    libX11
-    libXi
-    libXext
-    libXfixes
-    libXtst
-    sqlite
-    xorg.libxcb
-    pkgs.bash
+  fhsEnv = buildFHSEnv {
+    pname = "cassistant";
+    version = "6.5-1";
+
+    targetPkgs = pkgs: [
+      gtk2
+      glib
+      gdk-pixbuf
+      pango
+      cairo
+      atk
+      dbus
+      libX11
+      libXi
+      libXext
+      libXfixes
+      libXtst
+      sqlite
+      xorg.libxcb
+      pkgs.bash
+    ];
+
+    profile = ''
+      export INST_DIR=${extracted}/opt/assistant
+      export PATH=$INST_DIR/bin:$PATH
+    '';
+
+    runScript = "${extracted}/opt/assistant/bin/assistant";
+  };
+
+  desktopItem = makeDesktopItem {
+    name = "cassistant";
+    desktopName = "Ассистент";
+    comment = "Remote desktop and administration tool";
+    exec = "${fhsEnv}/bin/cassistant";
+    icon = "assistant";
+    terminal = false;
+    type = "Application";
+    categories = [ "Network" ];
+  };
+in symlinkJoin {
+  name = "cassistant-6.5-1";
+  paths = [
+    fhsEnv
+    (stdenv.mkDerivation {
+      pname = "cassistant-desktop";
+      version = "6.5-1";
+      dontUnpack = true;
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out/share/icons/hicolor/256x256/apps
+        cp ${extracted}/opt/assistant/share/icons/assistant.png \
+           $out/share/icons/hicolor/256x256/apps/assistant.png
+
+        mkdir -p $out/share/applications
+        cp ${desktopItem}/share/applications/cassistant.desktop $out/share/applications/
+
+        runHook postInstall
+      '';
+    })
   ];
-
-  profile = ''
-    export INST_DIR=${extracted}/opt/assistant
-    export PATH=$INST_DIR/bin:$PATH
-  '';
-
-  runScript = "${extracted}/opt/assistant/bin/assistant";
 
   meta = {
     description = "Ассистент - remote desktop and administration tool";
